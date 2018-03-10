31ee75a658db4035d11ab3e94c49339e3bfd8ff8
Matt Brigida <matt@complete-markets.com>
2018-03-10 10:58:30 -0500

    initial commit

---
+++ second_pop_macro.el
@@ -0,0 +1,4 @@
+(fset 'second_pop
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 44 14 1 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 backspace 44 14 1 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 backspace 44 14 1 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 backspace 44 14 1 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 backspace 44 14 1 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 backspace 44 14 1 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 backspace 44 14 1 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 backspace 44 14 1 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 backspace 44 14 1 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 backspace 44 14 1 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 backspace 44] 0 "%d")) arg)))
+(global-set-key [24 11 50] 'second_pop)
+(global-set-key [f1 11 50] 'second_pop)
