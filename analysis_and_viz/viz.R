8ed4639e18228013cb6954f0885fd19086eb4354
Matt Brigida <mdbrigida@gmail.com>
2018-03-31 13:27:27 -0400

    ggplot working

--- analysis_and_viz/viz.R
+++ analysis_and_viz/viz.R
@@ -2,7 +2,20 @@
 ## try working in the tidy framework------
 library(tidyverse)
 
-data <- read_csv("../ids_and_tracts.csv")
+## library(ggplot2)
 
+demData <- read_csv(url("https://github.com/matt-brigida/mdi_demographic_data/raw/master/ids_and_tracts.csv"))#, stringsAsFactors = FALSE)
+## demData <- demData[complete.cases(demData), ]
 
+demData$X2017.Estimated.Tract.Median.Family.Income <- as.numeric(gsub("\\$", "", demData$X2017.Estimated.Tract.Median.Family.Income))
 
+names(demData)[17] <- "tract_minority_percent"
+names(demData)[24] <- "percent_below_poverty_line"
+
+ggplot(demData, aes(tract_minority_percent)) +
+    geom_histogram()
+
+ggplot(demData, aes(percent_below_poverty_line)) +
+    geom_histogram()
+
+ggplot(demData, aes(y = tract_minority_percent, x = percent_below_poverty_line)) + geom_point()
